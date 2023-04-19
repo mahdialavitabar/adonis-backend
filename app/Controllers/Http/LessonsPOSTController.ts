8906660c89513f52d4d1d1a24e078839723b5400
davioncoder <130570744+davioncoder@users.noreply.github.com>
2023-04-19 22:52:36 +0330

    asdasdadd

--- app/Controllers/Http/LessonsPOSTController.ts
+++ app/Controllers/Http/LessonsPOSTController.ts
@@ -1,14 +1,35 @@
 // import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
+import { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 import Database from "@ioc:Adonis/Lucid/Database";
 
-export default class LessonsController {
-  public async lessons({ request, response }) {
+export default class ProjectsController {
+  public async lessons({ request, response }: HttpContextContract) {
     const lessons = request.body();
-    try {
-      await Database.table("lessons").insert({ ...lessons, id: lessons.id });
-    } catch (err) {
-      response.badRequest(err.messages);
+    const name = request.only(["name"]);
+
+    // Check if a project with the same purpose already exists
+    const existingProject = await Database.from("lessons")
+      .where("name", name.name)
+      .first();
+
+    if (existingProject) {
+      return response
+        .status(400)
+        .json({ error: "A project with this purpose already exists" });
+    } else {
+      // Insert the new project into the database
+      try {
+        await Database.table("lessons").insert({
+          ...lessons,
+          id: lessons.id,
+        });
+        return response
+          .status(200)
+          .json({ message: "Project added successfully" });
+      } catch (err) {
+        response.badRequest(err.messages);
+      }
     }
   }
 }
--- app/Controllers/Http/ProjectsPOSTController.ts
+++ app/Controllers/Http/ProjectsPOSTController.ts
@@ -1,14 +1,35 @@
 // import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
+import { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 import Database from "@ioc:Adonis/Lucid/Database";
 
 export default class ProjectsController {
-  public async projects({ request, response }) {
+  public async projects({ request, response }: HttpContextContract) {
     const projects = request.body();
-    try {
-      await Database.table("projects").insert({ ...projects, id: projects.id });
-    } catch (err) {
-      response.badRequest(err.messages);
+    const purpose = request.only(["purpose"]);
+
+    // Check if a project with the same purpose already exists
+    const existingProject = await Database.from("projects")
+      .where("purpose", purpose.purpose)
+      .first();
+
+    if (existingProject) {
+      return response
+        .status(400)
+        .json({ error: "A project with this purpose already exists" });
+    } else {
+      // Insert the new project into the database
+      try {
+        await Database.table("projects").insert({
+          ...projects,
+          id: projects.id,
+        });
+        return response
+          .status(200)
+          .json({ message: "Project added successfully" });
+      } catch (err) {
+        response.badRequest(err.messages);
+      }
     }
   }
 }
