59d063a753296766984535fdb10a873bfda86154
davioncoder <130570744+davioncoder@users.noreply.github.com>
2023-05-13 11:05:44 +0330

    kjkjkjhkjhjkhjklkjh

--- app/Controllers/Http/LessonsGETController.ts
+++ app/Controllers/Http/LessonsGETController.ts
@@ -3,7 +3,7 @@
 import Database from "@ioc:Adonis/Lucid/Database";
 
 export default class LessonsController {
-  public async lessons({ view }) {
+  public async lessons() {
     const lessons = await Database.from("lessons").select("*");
     return lessons;
   }
